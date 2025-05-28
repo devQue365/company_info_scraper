cd967ea59073726567648554bcb77d9f20dbb2d0
Devansh Rathore <devanshr2810@gmail.com>
2025-05-28 16:32:49 +0530

    Added career jet API and made job_salary_data_api output corrections

--- app/salary.py
+++ app/salary.py
@@ -1,4 +1,5 @@
 import http.client
+import requests
 import urllib.parse
 from pprint import pprint
 import json
@@ -137,11 +138,11 @@ def job_salary_data_api(company, job_title, location, API_KEY=None):
     res = json.loads(data)
     salary_data = res['data'][0]
     return {
-        'company': salary_data['company'],
+        'employer': salary_data['company'],
         'job_title': salary_data['job_title'],
         'location': salary_data['location'],
-        'salary_currency': salary_data['salary_currency'],
-        'salary_period': salary_data['salary_period'],
+        'currency': salary_data['salary_currency'],
+        'period': salary_data['salary_period'],
         'salary_range' : f'{salary_data['salary_currency']} {salary_data['min_salary']} - {salary_data['salary_currency']} {salary_data['max_salary']}',
         'median_salary': f'{salary_data['salary_currency']} {salary_data['median_salary']}',
         'mean_salary': f'{salary_data['salary_currency']} {round(salary_data['result']['totalPayStatistics']['mean'])}',
@@ -151,3 +152,98 @@ def job_salary_data_api(company, job_title, location, API_KEY=None):
         # 'max_salary': salary_data['max_salary'],
         # 'salary_count': salary_data['salary_count'],
     }
+
+# CareerJet API - 1000 requests / hour
+def carrer_jet_api(company, job_title, location):
+    try:
+        url = "http://public.api.careerjet.net/search"
+
+        params = {
+            "locale_code": "en_US",
+            "keywords": f"{company} {job_title}",
+            "location": location,
+            "affid": "09a7e99d93482b93310a29944476cd33",  
+            "user_ip": "1.2.3.4",        
+            "user_agent": "Mozilla/5.0", 
+            "pagesize": 10,              
+            "page": 1
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        # if there are no jobs based on the request made
+        if "jobs" not in data or not data["jobs"]:
+            return {"error": "No job data found"}
+        # If we got job data then let us filter it
+        filtered_jobs = [job for job in data.get("jobs", [])if job.get("company", "").lower() == company.lower()]
+        # if no filtered jobs exist
+        if not filtered_jobs:
+            return {"error": "No job data found"}
+        # else replace with the filtered jobs
+        data['jobs'] = filtered_jobs
+        def extract_job_summary(output_dict):
+            salaries = []
+            job_list = []
+            salary_period = {
+                'Y': 'YEAR',
+                'A': 'ANNUM',
+                'M': 'MONTH',
+                'W': 'WEEK',
+                'D': 'DAY',
+                'H': 'HOUR'
+            }
+            
+            for job in output_dict.get("jobs", []):
+                title = job.get("title", "")
+                job_list.append(title)
+                try:
+                    min_salary = float(job.get("salary_min", 0))
+                    max_salary = float(job.get("salary_max", 0))
+
+                    if min_salary:
+                        salaries.append(min_salary)
+                    if max_salary and max_salary != min_salary:
+                        salaries.append(max_salary)
+                except (ValueError, TypeError):
+                    continue
+            # if not salaries:
+            #     return {
+            #         'salary_range': 'N/A',
+            #         'base_salary': 'N/A',
+            #         'median_salary': 'N/A',
+            #         'mean_salary': 'N/A',
+            #     }
+            if(salaries):
+                salaries.sort()
+                currency = output_dict.get("salary_currency_code")
+                salary_range = f'{currency} {min(salaries):.2f} - {currency} {max(salaries):.2f}'
+                base_salary = f'{currency} {min(salaries):.2f}'
+                mean_salary = sum(salaries) / len(salaries)
+                median_salary = (
+                    salaries[len(salaries) // 2]
+                    if len(salaries) % 2 == 1
+                    else (salaries[len(salaries) // 2 - 1] + salaries[len(salaries) // 2]) / 2
+                )
+            else:
+                salary_range = None
+                base_salary = None
+                median_salary = None
+                mean_salary = None
+
+            job_info = {
+                "employer": next((p['company'] for p in output_dict.get('jobs', [])), None),
+                # "job_title": job.get("title", "").split(",")[0],  # I removed extra qualifiers like amazon-ADS
+                "job_title": job_list,
+                "location": next((p['locations'] for p in output_dict.get("jobs", [])), None),
+                "currency": next((p['salary_currency_code'] for p in output_dict.get("jobs", []) if p.get('salary_currency_code')), None),
+                "period": salary_period.get(next((p.get("salary_type") for p in output_dict.get("jobs", [])), None),None),
+                "salary_range": salary_range,
+                "base_salary": base_salary,
+                "median_salary": f'{currency} {median_salary:.2f}' if median_salary is not None else None,
+                "mean_salary": f'{currency} {mean_salary:.2f}' if mean_salary is not None else None,
+                "company_rating": None
+            }
+            return job_info
+        job_summary = extract_job_summary(data)
+        return job_summary
+    except Exception as e:
+        return {'error': str(e)}
