5c5f1e04b488689992c1ffc11f1c84d0d1c15bbf
Devansh Rathore <devanshr2810@gmail.com>
2025-06-04 20:08:51 +0530

    re-updated location.py with new api - gm__1 and gm__backup

--- app/location.py
+++ app/location.py
@@ -1,15 +1,77 @@
 import http.client
 import json
 import urllib.parse
-# 'Google Place Autocomplete and Place Info - 1000 requests / MO
-def gplace(company_name, location):
+
+# Threshold : 100 requests / Day (rich results)
+def gm__1(company, location):
+    try:
+        conn = http.client.HTTPSConnection("google-map-places.p.rapidapi.com")
+        headers = {
+            'x-rapidapi-key': "7ee1c65373msh181b4a01f21d239p1e5ff7jsnc2f07e071e6d",
+            'x-rapidapi-host': "google-map-places.p.rapidapi.com"
+        }
+        query_params = {
+            'query': f'{company},{location}',
+            'radius': 50000, # (50,000 m -> 50 km)
+            'language': 'en',
+            'type': 'establishment|point_of_interest',
+
+        }
+        # get url safe query
+        url_safe_query = urllib.parse.urlencode(query_params)
+        conn.request("GET", f"/maps/api/place/textsearch/json?{url_safe_query}", headers=headers)
+        res = conn.getresponse()
+        data = res.read()
+        output_dict = json.loads(data)
+        results = output_dict.get('results')
+        # initialize a container to hold results
+        container = []
+        for result in results:
+            # get the types
+            types = result.get('types',[])
+            # perform filtering
+            if any(['electronics_store' in types, 'store' in types]):
+                continue
+            else:
+                # get geometry key
+                geometry = result.get('geometry', {}) # return a dictionary
+                # get latitude and longitude values -> location field
+                location = geometry.get('location', {}) # return a dictionary
+                location_info = {
+                    # get business_status, place_id, plus_code, gloval_code
+                    'business_status': result.get('business_status', ''),
+                    'place_id': result.get('place_id', ''),
+                    'plus_code': result.get('plus_code', {}).get('compound_code', ''),
+                    'global_code': result.get('plus_code', {}).get('global_code', ''),
+                    # get name and address
+                    'name': result.get('name', ''),
+                    'address': result.get('formatted_address', ''),
+                    # get latitude and longitude, viewport (northeast and southwest)
+                    'location': location,
+                    'northeast': geometry.get('viewport', {}).get('northeast', {}),
+                    'southwest': geometry.get('viewport', {}).get('southwest', {}),
+                    # get open_now & types
+                    'open_now': result.get('opening_hours', {}).get('open_now', None),
+                    'rating': result.get('rating', ''),
+                    'user_ratings_total': result.get('user_ratings_total', ''),
+                    'type': types
+                }
+                container.append(location_info)
+        return container
+    except Exception as e:
+        return {'error': str(e)}
+
+
+# Threshold - 1000 requests / MO -> may/may not give all results [uses autocomplete predictions but will give relevant ones only]
+def gm__backup(company, location):
     conn = http.client.HTTPSConnection("google-place-autocomplete-and-place-info.p.rapidapi.com")
     headers = {
-        'x-rapidapi-key': "7ee1c65373msh181b4a01f21d239p1e5ff7jsnc2f07e071e6d",
+        'x-rapidapi-key': 
+        "7ee1c65373msh181b4a01f21d239p1e5ff7jsnc2f07e071e6d",
         'x-rapidapi-host': "google-place-autocomplete-and-place-info.p.rapidapi.com"
     }
     query_params = {
-        'input': f'{company_name} offices in {location}'
+        'input': f'{company},{location}'
     }
     # get the url-safe-query
     url_safe_query = urllib.parse.urlencode(query_params)
@@ -20,15 +82,20 @@ def gplace(company_name, location):
     output_dict = json.loads(data)
     def summarize():
         nonlocal output_dict
+        nonlocal query_params
         # initialize container
         container = []
         predictions = output_dict.get('predictions')
         for p in predictions:
-            description = p.get('description','')
-            place_id = p.get('place_id', '')
-            name = p.get('structured_formatting', {}).get('main_text', '')
-            address = p.get('structured_formatting', {}).get('secondary_text', '')
             types = p.get('types', [])
+            name = p.get('structured_formatting', {}).get('main_text', '').lower()
+            # filter out the results based on types and presence of required keyword
+            allowed_types = ['establishment', 'point_of_interest', 'premise', 'subpremise']
+            if(any(t not in allowed_types for t in types) or (company.lower() not in name)):
+                continue     
+            description = p.get('description','').lower()
+            place_id = p.get('place_id', '')
+            address = p.get('structured_formatting', {}).get('secondary_text', '').lower()
             job_info = {
                 'place_id': place_id,
                 'name': name,
@@ -37,50 +104,9 @@ def gplace(company_name, location):
                 'types': types,
             }
             container.append(job_info)
+
         return container
     return summarize()
 
-# Google map places (v2) - 100 requests / day
-def gmapv2(company_name, location):
-    conn = http.client.HTTPSConnection("google-map-places-new-v2.p.rapidapi.com")
-    payload = "{\"input\":\"" + company_name + "offices in" + location + "\",\"locationBias\":{\"circle\":{\"center\":{\"latitude\":40,\"longitude\":-110},\"radius\":10000}},\"includedPrimaryTypes\":[],\"includedRegionCodes\":[],\"languageCode\":\"\",\"regionCode\":\"\",\"origin\":{\"latitude\":0,\"longitude\":0},\"inputOffset\":0,\"includeQueryPredictions\":true,\"sessionToken\":\"\"}"
-    headers = {
-        'x-rapidapi-key': "7ee1c65373msh181b4a01f21d239p1e5ff7jsnc2f07e071e6d",
-        'x-rapidapi-host': "google-map-places-new-v2.p.rapidapi.com",
-        'Content-Type': "application/json",
-        'X-Goog-FieldMask': "*"
-    }
-    # request connection to the autocomplete endpoint
-    conn.request("POST", "/v1/places:autocomplete", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    output_dict = json.loads(data)
-    def summarize():
-        nonlocal output_dict
-        # container to hold data
-        container = []
-        suggestions = output_dict.get('suggestions')
-        for s in suggestions:
-            place_predictions = s.get('placePrediction', {})
-            text = place_predictions.get('text', {})
-            # get place_id
-            place_id = place_predictions.get('placeId', '')
-            # description
-            description = text.get('text', '')
-            # name
-            name = place_predictions.get('structuredFormat', {}).get('mainText', {}).get('text')
-            # address
-            address = place_predictions.get('structuredFormat', {}).get('secondaryText', {}).get('text')
-            # types
-            types = place_predictions.get('types', '')
-            # get job_info
-            job_info = {
-                'place_id': place_id,
-                'name': name,
-                'address': address,
-                'description': description,
-                'types': types,
-            }
-            container.append(job_info)
-        return container
-    return summarize()
+
+
