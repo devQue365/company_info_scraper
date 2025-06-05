8d0a9f653ba9522d66bb91dafbb65d11788cfe08
Devansh Rathore <devanshr2810@gmail.com>
2025-06-05 17:26:57 +0530

    Added twt__2

--- app/tweets.py
+++ app/tweets.py
@@ -17,7 +17,7 @@ def twt__1(company_name: str):
     query_params = {
         'type': 'Top',
         'count': 5,
-        'query': f"{company_name} company CEO's latest tweets"
+        'query': f"{company_name} ceo"
     }
     url_safe_query = urllib.parse.urlencode(query_params)
     # make a request to the Twitter API with the query parameters and headers
@@ -26,10 +26,10 @@ def twt__1(company_name: str):
     data = res.read()
     ouput_dict = json.loads(data.decode("utf-8"))
     
-    def extract_tweets(data):
+    def extract_tweets(output_dict):
         tweets = []
         # navigate through the timeline instructions
-        instructions = data.get('result', {}).get('timeline', {}).get('instructions', [])
+        instructions = output_dict.get('result', {}).get('timeline', {}).get('instructions', [])
         for instruction in instructions:
             entries = instruction.get('entries', [])
             for entry in entries:
@@ -76,3 +76,83 @@ def twt__1(company_name: str):
                         tweets.append(tweet_info)
         return tweets
     return extract_tweets(ouput_dict)
+
+# Threshold: 1000 Requests / MO
+def twt__2(company_name: str):
+    conn = http.client.HTTPSConnection("twitter-api45.p.rapidapi.com")
+    headers = {
+        'x-rapidapi-key': "7ee1c65373msh181b4a01f21d239p1e5ff7jsnc2f07e071e6d",
+        'x-rapidapi-host': "twitter-api45.p.rapidapi.com"
+    }
+    query_params = {
+        "query": f"{company_name} ceo",
+        "search_type": "Top"
+    }
+    # get url safe query
+    url_safe_query = urllib.parse.urlencode(query_params)
+    # request to the Twitter API with the query parameters and headers
+    conn.request("GET", f"/search.php?{url_safe_query}", headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    output_dict = json.loads(data)
+
+    def extract_tweets(output_dict):
+        tweets = []
+        # traverse  through the timeline to extract tweets
+        for tweet in output_dict.get("timeline", []):
+            if tweet.get("type") == "tweet":
+                # extract tweet information
+                tweet_info = {
+                    "tweet_id": tweet.get("tweet_id", ""),
+                    "text": tweet.get("text", ""),
+                    "created_at": tweet.get("created_at", ""),
+                    "screen_name": tweet.get("screen_name", ""),
+                    "source": tweet.get("source", ""),
+                    "lang": tweet.get("lang", ""),
+                    "conversation_id": tweet.get("conversation_id", ""),
+                    "retweets": tweet.get("retweets", 0),
+                    "replies": tweet.get("replies", 0),
+                    "quotes": tweet.get("quotes", 0),
+                    "favorites": tweet.get("favorites", 0),
+                    "bookmarks": tweet.get("bookmarks", 0),
+                    "views": tweet.get("views", "0"),
+                    "hashtags": [hashtag.get("text") for hashtag in tweet.get("entities", {}).get("hashtags", [])],
+                    "mentions": [
+                        {
+                            "id": mention.get("id_str", ""),
+                            "name": mention.get("name", ""),
+                            "screen_name": mention.get("screen_name", ""),
+                        }
+                        for mention in tweet.get("entities", {}).get("user_mentions", [])
+                    ],
+                    "urls": [
+                        {
+                            "url": url.get("url", ""),
+                            "expanded_url": url.get("expanded_url", ""),
+                            "display_url": url.get("display_url", ""),
+                        }
+                        for url in tweet.get("entities", {}).get("urls", [])
+                    ],
+                    "media": [
+                        {
+                            "type": media.get("type", ""),
+                            "url": media.get("media_url_https", ""),
+                            "video_info": media.get("video_info", {}),
+                            "sizes": media.get("sizes", {}),
+                        }
+                        for media in tweet.get("entities", {}).get("media", [])
+                    ],
+                    "user_info": {
+                        "screen_name": tweet.get("user_info", {}).get("screen_name", ""),
+                        "name": tweet.get("user_info", {}).get("name", ""),
+                        "description": tweet.get("user_info", {}).get("description", ""),
+                        "followers_count": tweet.get("user_info", {}).get("followers_count", 0),
+                        "friends_count": tweet.get("user_info", {}).get("friends_count", 0),
+                        "verified": tweet.get("user_info", {}).get("verified", False),
+                        "avatar": tweet.get("user_info", {}).get("avatar", ""),
+                    },
+                }
+                tweets.append(tweet_info)
+        return tweets
+    return extract_tweets(output_dict)
+
